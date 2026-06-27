ce30804304d2155179802a680e03ed80fc6a1376
cofico-dev <fr20210320@student.fon.bg.ac.rs>
2026-06-27 14:52:14 +0200

    Added register page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,10 +29,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      {/* Dodali smo 'relative' na body da bi video mogao da se zalepi u pozadinu */}
       <body className="bg-zinc-950 text-zinc-50 min-h-full flex flex-col font-sans relative overflow-x-hidden">
         
-        {/* VIDEO POZADINA */}
         <video 
           autoPlay 
           loop 
@@ -66,11 +64,9 @@ export default function RootLayout({
   }
 `}} />
 
-        {/* GLAVNI SADRŽAJ SA EFEKTOM STAKLA (Zamućeno i zamračeno) */}
         <div className="flex flex-col min-h-full w-full relative z-10 flex-1">
           <Navbar />
           
-          {/* bg-zinc-950/75 daje zamračenje od 75%, a backdrop-blur-lg daje ono jako zamućenje kroz prozor */}
           <main className="flex-1 flex flex-col bg-zinc-950/75 backdrop-blur-lg transition-all duration-300">
             {children}
           </main>
